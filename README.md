be38d0c6effb773bedd0867c54d96f9f5c949815
SpiralP <spiral.palmer@gmail.com>
2019-10-18 00:03:32 -0700

    add files

---
+++ README.md
@@ -0,0 +1,9 @@
+# rust-rodio-samplerate-bug
+
+Just run `cargo run`
+
+It's a little loud!
+
+`music-stereo.wav` is the original file, `music.wav` was converted to mono and exported from audacity at 16-bit PCM. `music.ogg` was then exported after
+
+Sound from http://www.music.helsinki.fi/tmt/opetus/uusmedia/esim/index-e.html
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use rodio::{
+  default_output_device, play_raw,
+  source::{ChannelVolume, Source},
+  Decoder,
+};
+use std::{fs::File, thread::sleep, time::Duration};
+
 fn main() {
-    println!("Hello, world!");
+  let file = File::open("music.ogg").unwrap();
+  let source = Decoder::new(file).unwrap();
+
+  // play louder in the right channel
+  let source = ChannelVolume::new(source, vec![0.2, 1.0]).convert_samples();
+
+  let device = default_output_device().unwrap();
+
+  play_raw(&device, source);
+
+  sleep(Duration::from_secs(20));
 }
